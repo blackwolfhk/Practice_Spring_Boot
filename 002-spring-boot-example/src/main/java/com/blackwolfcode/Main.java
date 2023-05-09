389c6be75a45b49a65c7f310f1a06756f8bc6c08
Black Wolf <76124063+blackwolfhk@users.noreply.github.com>
2023-05-09 11:46:54 +0800

    Update Main.java
    Code refactoring

--- 002-spring-boot-example/src/main/java/com/blackwolfcode/Main.java
+++ 002-spring-boot-example/src/main/java/com/blackwolfcode/Main.java
@@ -81,35 +81,3 @@ public class Main {
     }
 }
 
-//    class GreetResponse {
-//        private final String greet;
-//
-//        GreetResponse(String greet){
-//            this.greet = greet;
-//        }
-//    }
-//
-//        public String getGreet(){
-//            return greet;
-//        }
-//
-//        @Override
-//        public String toString() {
-//            return "GreetResponse{" +
-//                    "greet='" + greet + '\'' +
-//                    '}';
-//        }
-//
-//    @Override
-//    public boolean equals(Object o){
-//        if(this == o) return true;
-//        if (o == null || getClass() != o.getClass()) return false;
-//        GreetResponse that = (GreetResponse) o;
-//        return Objects.equals(greet, that.greet);
-//    }
-//
-//    @Override
-//    public int hashCode(){
-//        return Objects.hash(greet);
-//    }
-//}
