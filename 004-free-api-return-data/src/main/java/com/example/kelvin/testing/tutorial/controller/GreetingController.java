d86376ee76c1c1b1a719cf7e2d42bc3498861d1e
kelvincheung000 <cheungkamkit@gmail.com>
2023-05-17 23:09:07 +0800

    Updated developed API type

---
+++ 004-free-api-return-data/src/main/java/com/example/kelvin/testing/tutorial/controller/GreetingController.java
@@ -0,0 +1,111 @@
+package com.example.kelvin.testing.tutorial.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class GreetingController {
+
+    @RequestMapping("/testing")
+    public List<MyObject> getGreeting(){
+        List<MyObject> myObjects = new ArrayList<>();
+
+        MyObject obj1 = new MyObject();
+        obj1.setMethod("Testing API");
+        obj1.setMessage("Hello!");
+        obj1.setAuthor("Kelvin");
+
+        SubObject subObj1 = new SubObject();
+        subObj1.setDescription("This is a subobject.");
+        obj1.setSubObject(subObj1);
+
+        List<String> tags1 = new ArrayList<>();
+        tags1.add("tag1");
+        tags1.add("tag2");
+        obj1.setTags(tags1);
+
+        myObjects.add(obj1);
+
+        MyObject obj2 = new MyObject();
+        obj2.setMethod("Testing API");
+        obj2.setMessage("Hi there!");
+        obj2.setAuthor("Jane");
+
+        SubObject subObj2 = new SubObject();
+        subObj2.setDescription("This is another subobject.");
+        obj2.setSubObject(subObj2);
+
+        List<String> tags2 = new ArrayList<>();
+        tags2.add("tag3");
+        tags2.add("tag4");
+        obj2.setTags(tags2);
+
+        myObjects.add(obj2);
+
+        return myObjects;
+    }
+
+    public static class MyObject {
+        private String method;
+        private String message;
+        private String author;
+        private SubObject subObject;
+        private List<String> tags;
+
+
+        public String getMethod(){
+            return method;
+        }
+
+        public void setMethod(String method){
+            this.method = method;
+        }
+
+        public String getMessage(){
+            return message;
+        }
+
+        public void setMessage(String message){
+            this.message = message;
+        }
+
+        public String getAuthor(){
+            return author;
+        }
+
+        public void setAuthor(String author){
+            this.author = author;
+        }
+
+        public SubObject getSubObject() {
+            return subObject;
+        }
+
+        public void setSubObject(SubObject subObject){
+            this.subObject = subObject;
+        }
+
+        public List<String> getTags(){
+            return tags;
+        }
+
+        public void setTags(List<String> tags){
+            this.tags = tags;
+        }
+    }
+
+    public static class SubObject {
+        private String description;
+
+        public String getDescription(){
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+    }
+}
