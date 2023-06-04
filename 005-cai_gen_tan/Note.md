2fb533a41248454bccb5c36a49a8336e4d604ee9
kelvincheung000 <cheungkamkit@gmail.com>
2023-06-04 23:35:39 +0800

    Updated

--- 005-cai_gen_tan/Note.md
+++ 005-cai_gen_tan/Note.md
@@ -1,4 +1,5 @@
-## Import Data from Excel to Database
+## Import Data from Excel to PSQL Database
 
 ### A practice of doing migration via excel file with the context of "佛光菜根谭 Fo Guang Cai Gen tan"
 http://read.goodweb.net.cn/news/news_view.asp?newsid=114373
+
--- 005-cai_gen_tan/src/main/java/com/buddhist/cai_gen_tan/repository/BuddhistArticleRepository.java
+++ 005-cai_gen_tan/src/main/java/com/buddhist/cai_gen_tan/repository/BuddhistArticleRepository.java
@@ -2,7 +2,8 @@ package com.buddhist.cai_gen_tan.repository;
 
 import com.buddhist.cai_gen_tan.entity.BuddhistArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BuddhistArticleRepository extends JpaRepository<BuddhistArticle, Long> {
-    //    Add additional methods for custom queries if needed...
 }
