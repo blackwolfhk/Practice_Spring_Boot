b3d7b1a686ec876bb05afa31d820592d63b2a39e
kelvincheung000 <cheungkamkit@gmail.com>
2023-06-07 21:18:01 +0800

    Updated

--- 005-cai_gen_tan/src/main/java/com/buddhist/cai_gen_tan/repository/BuddhistArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.buddhist.cai_gen_tan.repository;
-
-import com.buddhist.cai_gen_tan.entity.BuddhistArticle;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BuddhistArticleRepository extends JpaRepository<BuddhistArticle, Long> {
-}
--- 005-cai_gen_tan/src/main/resources/db/migration/V1__create_articles_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE articles (
-    id SERIAL PRIMARY KEY,
-    chi_content TEXT,
-    eng_content TEXT
-);
